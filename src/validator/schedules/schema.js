79f140dbbea6aa29e2d5ff77ef239a570d516dc6
Husen <husenmalik7@gmail.com>
2025-11-20 03:46:32 +0700

    1

--- src/validator/schedules/schema.js
+++ src/validator/schedules/schema.js
@@ -1,8 +1,19 @@
 const Joi = require('joi');
 
 const SchedulePayloadSchema = Joi.object({
-  name: Joi.string().required(),
-  year: Joi.number().required(),
+  class_code: Joi.string().max(10).required(),
+  class_name: Joi.string().max(10).required(),
+  subject_code: Joi.string().max(10).required(),
+  teacher_nik: Joi.string().max(20).required(),
+  teacher_name: Joi.string().max(100).required(),
+  date: Joi.date().iso().required(),
+  jam_ke: Joi.number().integer().min(1).required(),
+  time_start: Joi.string()
+    .pattern(/^\d{2}:\d{2}:\d{2}$/)
+    .required(),
+  time_end: Joi.string()
+    .pattern(/^\d{2}:\d{2}:\d{2}$/)
+    .required(),
 });
 
 module.exports = { SchedulePayloadSchema };
