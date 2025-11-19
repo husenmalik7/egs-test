abe1057e1c38508e96a5b46fd94d4b94be30fd4b
Husen <husenmalik7@gmail.com>
2025-11-20 04:42:04 +0700

    1

--- src/api/schedules/routes.js
+++ src/api/schedules/routes.js
@@ -3,6 +3,9 @@ const routes = (handler) => [
     method: 'GET',
     path: '/api/schedules',
     handler: handler.getSchedulesHandler,
+    options: {
+      auth: 'api_key_auth',
+    },
   },
   {
     method: 'POST',
